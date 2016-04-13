60b1c03dade15c02a3d93fcb086398dcb02cd289
Mihai Alexandru Lupoiu <myhay.360@gmail.com>
2016-04-13 10:23:29 +0200

    Aded mex file.

---
+++ Project/fftGS_mex.c
@@ -0,0 +1,67 @@
+
+
+/*==========================================================
+ * arrayProduct.c - example in MATLAB External Interfaces
+ *
+ * Multiplies an input scalar (multiplier) 
+ * times a 1xN matrix (inMatrix)
+ * and outputs a 1xN matrix (outMatrix)
+ *
+ * The calling syntax is:
+ *
+ *		outMatrix = arrayProduct(multiplier, inMatrix)
+ *
+ * This is a MEX-file for MATLAB.
+ * Copyright 2007-2010 The MathWorks, Inc.
+ *
+ *========================================================*/
+/* $Revision: 1.1.10.3 $ */
+
+#include "mex.h"
+
+/* The computational routine */
+void fftGS_mex(double *x, mwSize n)
+{
+    mwSize i;
+    /* multiply each element y by x */
+    for (i=0; i<n; i++) {
+        x[i] = x[i] * 2;
+    }
+}
+
+/* The gateway function */
+void mexFunction( int nlhs, mxArray *plhs[],
+                  int nrhs, const mxArray *prhs[])
+{
+    double *inMatrix;               /* 1xN input matrix */
+    size_t ncols;                   /* size of matrix */
+    double *outMatrix;              /* output matrix */
+
+    /* check for proper number of arguments */
+    if(nrhs!=1) {
+        mexErrMsgIdAndTxt("MyToolbox:arrayProduct:nrhs","One input required.");
+    }
+     
+    /* check that number of rows in second input argument is 1 */
+    if(mxGetM(prhs[0])!=1) {
+        mexErrMsgIdAndTxt("MyToolbox:arrayProduct:notRowVector","Input must be a row vector.");
+    }
+    
+    /* create a pointer to the real data in the input matrix  */
+    inMatrix = mxGetPr(prhs[0]);
+
+    /* get dimensions of the input matrix */
+    ncols = mxGetN(prhs[0]);
+
+    /* create the output matrix */
+    plhs[0] = mxCreateDoubleMatrix(1,(mwSize)ncols,mxREAL);
+
+    /* get a pointer to the real data in the output matrix */
+    outMatrix = mxGetPr(plhs[0]);
+
+    memcpy(outMatrix, inMatrix, 1*ncols*mxGetElementSize(prhs[0]));
+    
+    /* call the computational routine */
+    fftGS_mex(outMatrix,(mwSize)ncols);
+}
+
